e60228effd889ed8e961134f3466d305a03e7bf5
thephisics101 <thephisics101@gmail.com>
2020-01-15 10:15:29 +0200

    Add React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Graph from "./components/Graph";
+
+import "./App.scss";
+
+export default class App extends React.Component {
+  render() {
+    return <div>
+      <Graph/>
+    </div>;
+  }
+}
--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default class Graph extends React.Component {
   render() {
-    return <div>Graph</div>;
+    return <div>I'm a Graph</div>;
   }
 }
